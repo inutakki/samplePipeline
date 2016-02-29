01221de67dac9632510c60183d5189ccfa32c501
inutakki <inutakki@yahoo.com>
2016-02-29 12:07:08 -0800

    Create pipeliencode.groovy

---
+++ CD-Pipeline/pipeliencode.groovy
@@ -0,0 +1,36 @@
+def configLoader;
+    
+    node{
+        stage 'setUp'
+           configLoader = fileLoader.fromGit('CD_ConfigProperties', 
+                    'https://github.com/inutakki/webtest.git', 'master', null, '')
+                    
+        stage 'checkout'
+       
+           git ([url:'https://github.com/inutakki/webtest.git'])
+           
+        stage 'BuildStep'
+        
+            def mvnHome=tool 'M3'
+            echo "${env.M2_HOME}"
+            echo "${createBuildString(configLoader)}"
+    }
+        
+            
+      
+    
+def createBuildString(ConfigLoaderObject){
+    
+def buildStep = "\${mvnHome}/bin/mvn -X clean deploy" +
+                    " -Pci -s "+ConfigLoaderObject.getSettingsFilePath()+
+                    " -DartifactoryURL=" +ConfigLoaderObject.getURL() +
+                    " -DartifactoryPublishUser=" + ConfigLoaderObject.getPublishUser() +
+                    " -DartifactoryPublishPassword="+ ConfigLoaderObject.getPublishUser() +
+                    " -DreleaseNumber=" + ConfigLoaderObject.getReleaseNumber() + "\"";
+    echo "${buildStep}";
+    return buildStep;
+
+
+    
+}
+
